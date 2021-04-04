44b73467869e8d503ea6ec7d0d00167dd1923c44
msh <mohammad.shetabivash@gmail.com>
2021-04-04 10:29:57 +0430

    md file conversion scaffold added

---
+++ _pages/dynamic/test.md
@@ -0,0 +1,21 @@
+---
+title: "Article title"
+description: "Article short description"
+timeToRead: "25"
+date: "2020-06-23T02:28:59Z"
+---
+
+# Heading 1
+
+A paragraph with some **bold text**.
+
+## This is a subtitle
+
+And a paragraph in next line
+
+![alt text](/test/listen-half.jpg)
+
+```
+cd linuxiha/first/code-section
+```
+
---
+++ pages/articles/[slug].js
@@ -0,0 +1,61 @@
+import React from 'react'
+import {useRouter} from 'next/router';
+import {getDynamicPageContentBySlug, getAllDynamicPages} from '../../src/lib/markdown-utils';
+import PrintMarkdown from '../../src/components/printMarkDown';
+
+export default function Article({page}) {
+    const router = useRouter();
+
+    const {
+        title,
+        description,
+        customTitle,
+        slug,
+        content,
+      } = page;
+    
+      return (
+        <div>
+          <h1>{title}</h1>
+          <h2>{customTitle}</h2>
+          {/* we'll go into the PrintMarkdown component later */}
+          <PrintMarkdown markdown={content} />
+        </div>
+      );
+}
+
+
+export async function getStaticProps({ params }) {
+    const { slug } = params;
+  
+    // Pass in the fields that we want to get
+    const page = getDynamicPageContentBySlug(slug, [
+      'title',
+      'description',
+      'customTitle',
+      'field',
+      'slug',
+      'content',
+    ]);
+  
+    return {
+      props: {
+        page: {
+          ...page,
+        },
+      },
+    };
+  }
+
+  export async function getStaticPaths() {
+    const posts = getAllDynamicPages(['slug']);
+    const paths = posts.map(({ slug }) => ({
+      params: {
+        slug,
+      },
+    }));
+    return {
+      paths,
+      fallback: false,
+    };
+  }
