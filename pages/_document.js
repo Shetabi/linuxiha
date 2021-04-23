5fe26cf8329b8fb5b9829e9c0c3900b2ee4b0c68
msh <mohammad.shetabivash@gmail.com>
2021-04-23 21:50:56 +0430

    manifest url fixed

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,11 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="manifest" href="/manifest.json"/>
-                    <link rel="apple-touch-icon" href="/icon-512x512.png"/>
-                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                    <link rel="manifest" href="/linuxiha/manifest.json"/>
+                    <link rel="apple-touch-icon" href="/linuxiha/icon-512x512.png"/>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/linuxiha/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/linuxiha/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/linuxiha/favicon-16x16.png"/>
                     <meta name="msapplication-TileColor" content="#ce1919"></meta>
                     <meta name="theme-color" content="#ce1919" />
                 </Head>
